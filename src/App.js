a0ef5986715f79bfc06c2b1d1defedc3272321c0
iamanas20 <anaslatique@gmail.com>
2018-12-16 16:14:40 +0100

    created repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/navbar/navbar.js'
+
+export default class App extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <NavBar/>
+         );
+    }
+}
+
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './navbar.css'
+
+export default class NavBar extends Component {
+    state = {  }
+    render() {  
+        return ( 
+            <div><h2>Hello World</h1><button>Click me!</button></div>
+         );
+    }
+}
