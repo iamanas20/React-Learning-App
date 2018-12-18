f2dc569bd7d7deab3137ba12c95296ce67b73903
iamanas20 <anaslatique@gmail.com>
2018-12-18 23:53:07 +0100

    moving on steps in components, transitions, props, state object

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/navbar/navbar.js'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Transition } from "react-transition-group";
 
-export default class App extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <NavBar/>
-         );
-    }
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.duration = 2000;
+    this.state = {
+      in: false
+    };
+    setTimeout(() => {
+      this.setState({
+        in: true
+      });
+    }, 2000);
+    setTimeout(() => {
+      this.setState({
+        in: false
+      });
+    }, 6000);
+  }
+  render() {
+    return (
+      <Transition in={this.state.in} timeout={this.duration}>
+        {status => {
+          return <h1>{status}</h1>;
+        }}
+      </Transition>
+    );
+  }
 }
-
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,15 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './navbar.css'
+import Transition from 'react-transition-group';
 
-export default class NavBar extends Component {
-    state = {  }
-    render() {  
-        return ( 
-            <div><h2>Hello World</h1><button>Click me!</button></div>
-         );
+export default class Moving(props) {
+    state = {
+        bla: '',
+        blo: '',
+    }
+    render{
+        return(
+            <h1 {...this.state}>Hello World</h1>
+        );
     }
 }
