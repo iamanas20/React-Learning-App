a1c47229d10568e6eeab2e0b56acddeec31598a4
iamanas20 <anaslatique@gmail.com>
2018-12-19 02:09:56 +0100

    learning array functions

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,14 +2,15 @@ import React, {Component, Fragment} from 'react';
 import './navbar.css'
 import Transition from 'react-transition-group';
 
-export default class Moving(props) {
+export default class Moving extends Component {
     state = {
-        bla: '',
+        my_array: [1, 2, 3],
+        my_second_array: [4, 5, 6],
         blo: '',
-    }
-    render{
-        return(
-            <h1 {...this.state}>Hello World</h1>
-        );
-    }
+    };
+    render() {
+        return <h1>
+            {this.state.my_array.find(number => number > 2)}
+          </h1>;
+    };
 }
